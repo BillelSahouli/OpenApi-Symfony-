c430f73e38f3dc6cedd55d3705c35cf0607cb24f
Billel <b.sahouli@hotmail.fr>
2021-12-09 12:32:37 +0100

    config de swagger & exemple requet Get BlogController

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use OpenApi\Annotations as OA;
+
+
+class BlogController extends AbstractController
+{
+    /**
+     * @OA\Get(
+     * path="/posts",
+     *      @OA\Response(
+     *          response = "200",
+     *          description="Nos articles",
+     *          @OA\JsonContent(type="string", description="Titre du premier article"),
+     *      )
+     * )
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
